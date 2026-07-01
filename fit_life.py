caeb151560dc705c98740b537c0aa68b3acea156
l0btuf <alexandr.chepkasov@gmail.com>
2026-07-01 22:48:25 +0300

    fit_life_sprint1_v2

--- fit_life.py
+++ fit_life.py
@@ -4,28 +4,34 @@ if sys.platform == "win32":
     sys.stdout.reconfigure(encoding="utf-8")
     sys.stdin.reconfigure(encoding="utf-8")
 
-# Проект FitLife - MVP версия 1.0
+# Проект FitLife - MVP версия 2.0
 
-print("Цифровой фитнес-трекер приветствует тебя")
+print("Цифровой фитнес-трекер приветствует вас")
 
 # 1. Знакомство
-user_name = input("Как я могу к тебе обращаться? ")
-user_age = int(input("Сколько вам лет? "))
+user_name = input("Как я могу к вам обращаться? ")
 
-# 2. Сбор данных
+# 2. Запуск цикла для проверки ввода возраста
+while True:
+    try:
+        user_age = int(input("Сколько вам лет? "))
+        break
+    except ValueError:
+        print("Пожалуйста, введите ваш возраст цифрами.\n")
+
+# 3. Сбор данных
 user_weight = float(input("Какой у вас вес (кг)? Например: 75.5 "))
 user_height = float(input("Какой у вас рост (м)? Например: 1.8 "))
 
-# 3. Расчет индекса массы тела
+# 4. Расчет индекса массы тела
 bmi = round(user_weight / (user_height ** 2), 1)
 
-# Переименовано строго по TODO задания: water_needed вместо water_liters
-water_needed = user_weight * 30 / 1000
+# 5. Расчет нормы воды
+water_per_kg = 30
+water_needed = user_weight * water_per_kg / 1000
 
-# 4. Вывод красивого результата
-print()
-print(f"Отчет для пользователя: {user_name} ({user_age} г.)")
-print(f"Твой Индекс Массы Тела: {bmi}")
-print(f"Рекомендуемая норма воды: {water_needed:.1f} л. в день")
-print()
+# 6. Вывод красивого результата
+print(f"Отчет для пользователя: {user_name} ({user_age} г.)\n")
+print(f"Ваш Индекс Массы Тела: {bmi}")
+print(f"Рекомендуемая норма воды: {water_needed:.1f} л. в день\n")
 print("Расчет окончен. Будьте здоровы!")
